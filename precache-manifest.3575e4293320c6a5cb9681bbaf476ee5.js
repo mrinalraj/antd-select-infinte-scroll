6dc9b7fdfe2967788de1fc7e183032cba096c802
Mrinal Raj <raj.mrnl@gmail.com>
2020-02-02 14:21:14 +0530

    Updates

---
+++ precache-manifest.3575e4293320c6a5cb9681bbaf476ee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac164dc5f786af39d1472525596e6cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7be5be4a2452eb05f0c8",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "df0a3eeff8e43032171a",
+    "url": "/static/css/main.25f2b575.chunk.css"
+  },
+  {
+    "revision": "7be5be4a2452eb05f0c8",
+    "url": "/static/js/2.30c85ed9.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.30c85ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0a3eeff8e43032171a",
+    "url": "/static/js/main.3ae560a4.chunk.js"
+  },
+  {
+    "revision": "70d5329c96c7ff4903fa",
+    "url": "/static/js/runtime-main.f2ac844f.js"
+  }
+]);
